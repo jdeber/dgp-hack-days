e7628e49fc3ad2c63252dd96bc93042d0cfba33c
frank.cy.li <frank.cy.li@gmail.com>
2012-03-02 23:40:18 +0000

    New Webcam library

--- FreeFoodButton/FreeFoodButton/MainWindow.xaml.cs
+++ FreeFoodButton/FreeFoodButton/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Shapes;
 using System.Windows.Interop;
 using System.Runtime.InteropServices;
 using System.Windows.Threading;
+using System.Collections.ObjectModel;
+using System.IO;
 
 namespace FreeFoodButton
 {
@@ -25,8 +27,8 @@ namespace FreeFoodButton
         private const int WM_SYSCOMMAND     = 0x112;
         private const int SC_SCREENSAVE     = 0xF140;
         private const int SC_MONITORPOWER   = 0xF170;
-        private WebCam webcam;
-        private DispatcherTimer dispatcherTimer;        
+        private DispatcherTimer dispatcherTimer;
+        private string m_filename = "C:\\inetpub\\wwwroot\\FreeFood\\Food.jpg";
 
         public MainWindow()
         {
@@ -48,13 +50,96 @@ namespace FreeFoodButton
             Log.Write("Program Starting", Log.Categories.System);
 
             //Web cam code
-            webcam = new WebCam();
-            webcam.Start();
+            // Create default device
+            SelectedWebcamMonikerString = (CapDevice.DeviceMonikers.Length > 0) ? CapDevice.DeviceMonikers[0].MonikerString : "";
+
             dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
             dispatcherTimer.Start();           
         }
+
+        #region Properties
+        /// <summary>
+        /// Wrapper for the WebcamRotation dependency property
+        /// </summary>
+        public double WebcamRotation
+        {
+            get { return (double)GetValue(WebcamRotationProperty); }
+            set { SetValue(WebcamRotationProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for WebcamRotation.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WebcamRotationProperty =
+            DependencyProperty.Register("WebcamRotation", typeof(double), typeof(MainWindow), new UIPropertyMetadata(180d));
+
+        /// <summary>
+        /// Wrapper for the SelectedWebcam dependency property
+        /// </summary>
+        public CapDevice SelectedWebcam
+        {
+            get { return (CapDevice)GetValue(SelectedWebcamProperty); }
+            set { SetValue(SelectedWebcamProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedWebcam.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedWebcamProperty =
+            DependencyProperty.Register("SelectedWebcam", typeof(CapDevice), typeof(MainWindow), new UIPropertyMetadata(null));
+
+        /// <summary>
+        /// Wrapper for the SelectedWebcamMonikerString dependency property
+        /// </summary>
+        public string SelectedWebcamMonikerString
+        {
+            get { return (string)GetValue(SelectedWebcamMonikerStringProperty); }
+            set { SetValue(SelectedWebcamMonikerStringProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedWebcamMonikerString.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedWebcamMonikerStringProperty = DependencyProperty.Register("SelectedWebcamMonikerString", typeof(string),
+            typeof(MainWindow), new UIPropertyMetadata("", new PropertyChangedCallback(SelectedWebcamMonikerString_Changed)));
+
+        /// <summary>
+        /// Wrapper for the SelectedImages dependency property
+        /// </summary>
+        public ObservableCollection<BitmapSource> SelectedImages
+        {
+            get { return (ObservableCollection<BitmapSource>)GetValue(SelectedImagesProperty); }
+            set { SetValue(SelectedImagesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedImages.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedImagesProperty = DependencyProperty.Register("SelectedImages", typeof(ObservableCollection<BitmapSource>),
+            typeof(MainWindow), new UIPropertyMetadata(new ObservableCollection<BitmapSource>()));
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Invoked when the SelectedWebcamMonikerString dependency property has changed
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">EventArgs</param>
+        private static void SelectedWebcamMonikerString_Changed(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Get typed sender
+            MainWindow typedSender = sender as MainWindow;
+            if (typedSender != null)
+            {
+                // Get new value
+                string newMonikerString = e.NewValue as string;
+
+                // Update the device
+                if (typedSender.SelectedWebcam == null)
+                {
+                    // Create it
+                    typedSender.SelectedWebcam = new CapDevice("");
+                }
+
+                // Now set the moniker string
+                typedSender.SelectedWebcam.MonikerString = newMonikerString;
+            }
+        }
+        #endregion
 
         private static IntPtr ScreenSaverHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
@@ -68,12 +153,18 @@ namespace FreeFoodButton
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            webcam.SaveImage();
+            using (var fileStream = new FileStream(m_filename, FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(player.CurrentBitmap));
+                encoder.Save(fileStream);
+                encoder = null;
+            }
         }
 
         protected override void OnClosed(EventArgs e)
         {
-            webcam.Stop();
+            //webcam.Stop();
             dispatcherTimer.Stop();
             base.OnClosed(e);
         }
@@ -86,5 +177,26 @@ namespace FreeFoodButton
         public static extern uint SetThreadExecutionState(uint esFlags);
         public const uint ES_CONTINUOUS = 0x80000000;
         public const uint ES_SYSTEM_REQUIRED = 0x00000001;
+    }
+
+    public class ScaleConverter : IMultiValueConverter
+    {
+
+
+        #region IMultiValueConverter Members
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            float v = (float)values[0];
+            double m = (double)values[1];
+            return v * m / 50;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
